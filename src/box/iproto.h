e500b5cdc6eb5156aee9cd037cad1a84609f9072
mechanik20051988 <mechanik20.05.1988@gmail.com>
2021-06-22 13:02:54 +0300

    iproto: implement ability to open several listening sockets
    Previously, iproto could only open only one socket for listening.
    This patch implement ability to open up to 20 different listening
    sockets.
    Part of #5928

--- src/box/iproto.h
+++ src/box/iproto.h
@@ -32,6 +32,9 @@
  */
 
 #include <stddef.h>
+#include <stdint.h>
+
+#include "sio.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -52,6 +55,15 @@ enum {
 	 * processing stops until some new fibers are freed up.
 	 */
 	IPROTO_FIBER_POOL_SIZE_FACTOR = 5,
+	/** Maximum count of listening sockets */
+	IPROTO_LISTEN_SOCKET_MAX = 20,
+	/**
+	 * We need `SERVICE_NAME_MAXLEN` bytes for each listen
+	 * address and two bytes for delimiter `,` between them.
+	 */
+	IPROTO_LISTEN_INFO_MAXLEN =
+		(SERVICE_NAME_MAXLEN + 2) * IPROTO_LISTEN_SOCKET_MAX,
+
 };
 
 extern unsigned iproto_readahead;
@@ -97,7 +109,7 @@ void
 iproto_init(int threads_count);
 
 int
-iproto_listen(const char *uri);
+iproto_listen(const char **uri_array, int size);
 
 void
 iproto_set_msg_max(int iproto_msg_max);
