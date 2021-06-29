a80c318cb8769999a3981039d536e9f1ded926c2
Cyrill Gorcunov <gorcunov@gmail.com>
2021-06-29 15:41:37 +0300

    test
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- test/replication/gh-6036-rollback-confirm.test.lua
+++ test/replication/gh-6036-rollback-confirm.test.lua
@@ -9,7 +9,10 @@ test_run:cmd('create server replica with script="replication/gh-6036-replica.lua
 test_run:cmd('start server master')
 test_run:cmd('start server replica')
 
--- step 1 & 2
+--
+-- Connect master to the replica and write a record. Since the quorum
+-- value is bigger than number of nodes in a cluster it will be rolled
+-- back later.
 test_run:switch('master')
 box.cfg({                                       \
     replication = {                             \
@@ -19,38 +22,45 @@ box.cfg({                                       \
 })
 _ = box.schema.create_space('sync', {is_sync = true})
 _ = box.space.sync:create_index('pk')
-box.info()
 
--- step 3
+--
+-- Wait the record to appear on the master.
 f = require('fiber').create(function() box.space.sync:replace{1} end)
+test_run:wait_cond(function() return box.space.sync:get({1}) ~= nil end, 100)
 box.space.sync:select{}
 
+--
+-- Wait the record from master get written and then
+-- drop the replication.
 test_run:switch('replica')
--- step 4
+test_run:wait_cond(function() return box.space.sync:get({1}) ~= nil end, 100)
+box.space.sync:select{}
 box.cfg{replication = {}}
-box.info()
 
+--
+-- Then we jump back to the master and drop the replication,
+-- thus unconfirmed record get rolled back.
 test_run:switch('master')
--- step 5
 box.cfg({                                       \
     replication = {},                           \
     replication_synchro_timeout = 0.001,        \
     election_mode = 'manual',                   \
 })
-box.info()
-while f:status() ~= 'dead' do fiber.sleep(0.1) end
-box.space.sync:select{}
+while f:status() ~= 'dead' do require('fiber').sleep(0.1) end
+test_run:wait_cond(function() return box.space.sync:get({1}) == nil end, 100)
 
+--
+-- Force the replica to become a RAFT leader.
 test_run:switch('replica')
---step 6
 box.cfg({                                       \
     replication_synchro_quorum = 1,             \
     election_mode = 'manual'                    \
 })
 box.ctl.promote()
 box.space.sync:select{}
-box.info()
 
+--
+-- Connect master back to the replica.
 test_run:switch('master')
 box.cfg({                                       \
     replication = {                             \
@@ -58,7 +68,18 @@ box.cfg({                                       \
             "unix/:./replica.sock",             \
     },                                          \
 })
-box.info()
+box.space.sync:select{}
+
+--
+-- Connect replica back to the master.
+test_run:switch('replica')
+box.cfg({                                       \
+    replication = {                             \
+            "unix/:./master.sock",              \
+            "unix/:./replica.sock",             \
+    },                                          \
+})
+box.space.sync:select{}
 
 test_run:switch('default')
 test_run:cmd('stop server master')
